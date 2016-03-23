7275bf8295154d56fd41762a695e6e129ef96748
Jose Pablo Granados <jpgcodecr@gmail.com>
2016-03-23 15:51:29 -0600

    Fixing conditional big

--- mobileDetect.js
+++ mobileDetect.js
@@ -6,7 +6,7 @@ Author: Jose Pablo Granados
 var breakpoint = 768;
 
 var isMobile = function(){
-	if(window.matchMedia !=== undefined){
+	if(window.matchMedia !== undefined){
 		return window.matchMedia("(max-width: "+ breakpoint +"px)").matches
 	}else{
 		var windowWidth = $(window).width();
