e65b75c2f9bed6ccddd279369251d170b6fc2ce2
Jose Pablo Granados <jpgcodecr@gmail.com>
2016-03-25 13:43:27 -0600

    Improving error handling

--- responsiveMatch.js
+++ responsiveMatch.js
@@ -1,8 +1,6 @@
 var responsiveMatch = function(options){
 
-	var breakpoints = { mobile: 767, tablet: 1023, desktop: 1279 },
-		breakpoint,
-		query;
+	var breakpoints = { mobile: 767, tablet: 1023, desktop: 1279 }, breakpoint, query, wrongParam = false;
 
 	if(typeof(options) === 'string'){
 		
@@ -12,18 +10,27 @@ var responsiveMatch = function(options){
 			query = '(max-width: '+ breakpoint +'px)';
 		}else if(options === 'tablet'){
 			query = '(min-width: '+ breakpoints['mobile'] +'px) and (max-width: '+ breakpoints[options] +'px)';
-		}else{
+		}else if(options === 'desktop'){
 			query = '(min-width: '+ breakpoints['tablet'] +'px)';
+		}else{
+			wrongParam = true;
 		}
+
 	}else if(typeof(options) === 'number'){
 		breakpoint = options;
 	}else{
-		console.error('You are passing an invalid parameter, please check responsiveMatch docs');
+		wrongParam = true;		
 	}
 
 
 	if(window.matchMedia !== undefined){
-		return window.matchMedia(query).matches
+		if(wrongParam !== true){
+			return window.matchMedia(query).matches;
+		}else{
+			return 'You are passing an invalid parameter, please check responsiveMatch docs';
+		}
+	}else{
+		return 'You are passing an invalid parameter, please check responsiveMatch docs';
 	}
 
 }
